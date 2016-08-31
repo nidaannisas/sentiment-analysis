011e57d84ca838d18bc2f607963b299b288114f3
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-08-31 22:23:31 +0700

    Create table bag-of-words

---
+++ database/migrations/2016_08_31_151544_create_bag_of_words_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBagOfWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bag-of-words', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('word');
+            $table->float('idf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
