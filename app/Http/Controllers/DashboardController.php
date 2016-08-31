4333033314d0ae5b48e8e6db51e431fbaa7cba96
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-01 03:29:46 +0700

    Add dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DashboardController extends Controller
+{
+    use FilterTemplate, UserTemplate;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function index()
+    {
+
+    }
+
+}
