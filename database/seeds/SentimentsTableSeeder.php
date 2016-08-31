dff0684ad2942555702520b2c90e7a1d35a42041
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-01 03:15:25 +0700

    Sentiments table seeder

---
+++ database/seeds/SentimentsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SentimentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
