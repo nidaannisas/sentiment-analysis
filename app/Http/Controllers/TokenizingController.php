cd18c522f58a700836c7b9a5ba79245492ac449b
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-01 21:02:29 +0700

    View tokenizing

--- app/Http/Controllers/TokenizingController.php
+++ app/Http/Controllers/TokenizingController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\BagOfWord;
 use App\Http\Requests;
 
 class TokenizingController extends Controller
 {
     public function index()
     {
+    	$words = BagOfWord::all();
 
+    	return view('tokenizing.index')
+    		->with('words', $words);
     }
 
     public function tokenize()
     {
-    	
+
     }
 }
---
+++ resources/views/tokenizing/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layout')
+
+@section('content')
+
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+	<div class="row">
+		<ol class="breadcrumb">
+			<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+			<li class="active">Tweets</li>
+		</ol>
+	</div><!--/.row-->
+	
+	<div class="row" style="margin-top : 20px;">
+		<div class="col-lg-12">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="panel-heading">Tokenizing</div>
+					<div class="col-md-6">
+						<h3>Tokenizing</h3>
+						<p>Proses untuk mengubah data training ke token.</p>
+
+						<button class="btn btn-success"><span class="glyphicons glyphicons-disk-open"></span> Process</button>
+					</div>
+					<div class="col-md-6" style="padding-bottom: 20px;">
+						
+					</div>
+					<hr style="color: black; width: 100%;">
+					<div class="col-md-12">
+						<table data-toggle="table"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
+						    <thead>
+						    <tr>
+						        <th data-field="state" data-checkbox="true">Item ID</th>
+						        <th data-field="name"  data-sortable="true" style="width: 80%;"">Word</th>
+						    </tr>
+						    </thead>
+						    <tbody>
+						    @foreach($words as $word)
+						    <tr>
+						    	<td>{{ $word->id }}</td>
+						    	<td>{{ $word->word }}</td>
+							</tr>
+							@endforeach
+						    </tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!--/.row-->	
+	
+	
+</div><!--/.main-->
+
+<link href="{{ URL::to('css/bootstrap-table.css') }}" rel="stylesheet">
+
+<script src="{{ URL::to('js/jquery-1.11.1.min.js') }}"></script>
+<script src="{{ URL::to('js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::to('js/bootstrap-datepicker.js') }}"></script>
+<script src="{{ URL::to('js/bootstrap-table.js') }}"></script>
+<script>
+	!function ($) {
+		$(document).on("click","ul.nav li.parent > a > span.icon", function(){		  
+			$(this).find('em:first').toggleClass("glyphicon-minus");	  
+		}); 
+		$(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+	}(window.jQuery);
+
+	$(window).on('resize', function () {
+	  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+	})
+	$(window).on('resize', function () {
+	  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+	})
+</script>	
+
+@stop
