c8449cd46e7efd30eaab2c7c79742d9b746452bc
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-02 04:36:14 +0700

    bag of words model serarch function

--- app/Models/BagOfWord.php
+++ app/Models/BagOfWord.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use DB;
+
 Class BagOfWord extends Model
 {
     protected $table = 'bag-of-words';
+
+    public static function search($word)
+    {
+        $data   = DB::table('bag-of-words')
+                ->where('word', $word)
+                ->get();
+    
+        if(!empty($data))
+            return $data[0];
+        else
+            return $data;
+    }
 }
