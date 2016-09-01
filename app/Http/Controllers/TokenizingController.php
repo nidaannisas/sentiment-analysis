ba9f2abba8e415893deef91d05aaa0d020d499c2
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-01 21:04:51 +0700

    tokenizing function draft

--- app/Http/Controllers/TokenizingController.php
+++ app/Http/Controllers/TokenizingController.php
@@ -10,14 +10,27 @@ class TokenizingController extends Controller
 {
     public function index()
     {
-    	$words = BagOfWord::all();
+        $words = BagOfWord::all();
 
-    	return view('tokenizing.index')
-    		->with('words', $words);
+        return view('tokenizing.index')
+            ->with('words', $words);
     }
 
     public function tokenize()
     {
+        $content = "This is a test string, which is used for
 
+        demonstrating the tokenization using PHP. PHP is a very (strong) scripting-language";
+
+        $words = array();
+        $delim = " \n.,;-()";
+        $tok = strtok($content, $delim);
+        while ($tok !== false) {
+          $words[] = $tok;
+          $tok = strtok($delim);
+        }
+        $unique_words = array_unique($words);
+
+        var_dump($unique_words);
     }
 }
