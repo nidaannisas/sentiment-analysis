10e86c4560889ce8298339a59fead3912bec36c4
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-02 05:06:04 +0700

    word normalization sidebar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -56,6 +56,7 @@
 			<li class="<?php if (Request::is('dashboard/tweets')) echo 'active'; ?>"><a href="{{ URL::to('dashboard/tweets') }}"><svg class="glyph stroked empty message"><use xlink:href="#stroked-empty-message"/></svg> Tweets</a></li>
 			<li class="<?php if (Request::is('dashboard/tokenizing')) echo 'active'; ?>"><a href="{{ URL::to('dashboard/tokenizing') }}"><svg class="glyph stroked chain"><use xlink:href="#stroked-chain"/></svg> Tokenizing</a></li>
 			<li class="<?php if (Request::is('dashboard/stopwords')) echo 'active'; ?>"><a href="{{ URL::to('dashboard/stopwords') }}"><svg class="glyph stroked trash"><use xlink:href="#stroked-trash"/></svg> Stopwords</a></li>
+			<li class="<?php if (Request::is('dashboard/normalization')) echo 'active'; ?>"><a href="{{ URL::to('dashboard/normalization') }}"><svg class="glyph stroked trash"><use xlink:href="#stroked-trash"/></svg> Word Normalization</a></li>
 		</ul>
 
 	</div><!--/.sidebar-->
