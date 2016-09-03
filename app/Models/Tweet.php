4c5241ca49c44aa2335e979e3390e1ceb8fef1e7
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-03 21:37:47 +0700

    tweet model add function count posoteve & negative & neutral

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -13,4 +13,34 @@ Class Tweet extends Model
         return $this->belongsTo(Sentiment::class, 'sentiment_id');
     }
 
+    public static function countPositive()
+    {
+        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tweets` WHERE sentiment_id = 1"));
+    
+        if(!empty($data))
+            return $data[0]->count;
+        else
+            return $data;
+    }
+
+    public static function countNegative()
+    {
+        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tweets` WHERE sentiment_id = 2"));
+    
+        if(!empty($data))
+            return $data[0]->count;
+        else
+            return $data;
+    }
+
+    public static function countNeutral()
+    {
+        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tweets` WHERE sentiment_id = 3"));
+    
+        if(!empty($data))
+            return $data[0]->count;
+        else
+            return $data;
+    }
+
 }
