b79460bc93cdd99e821beade5fbaf5e156c53552
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-03 19:03:04 +0700

    add function count per sentiment

--- app/Models/BagOfWord.php
+++ app/Models/BagOfWord.php
@@ -35,4 +35,43 @@ Class BagOfWord extends Model
         else
             return $data;
     }
+
+    public static function countPositive($token_id)
+    {
+        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 1 AND `tweets`.`id` = `tdm`.`tweet_id`"),
+                    array(
+                        "token_id"    => $token_id
+                    ));
+    
+        if(!empty($data))
+            return $data[0]->count;
+        else
+            return $data;
+    }
+
+    public static function countNegative($token_id)
+    {
+        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 2 AND `tweets`.`id` = `tdm`.`tweet_id`"),
+                    array(
+                        "token_id"    => $token_id
+                    ));
+    
+        if(!empty($data))
+            return $data[0]->count;
+        else
+            return $data;
+    }
+
+    public static function countNeutral($token_id)
+    {
+        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 3 AND `tweets`.`id` = `tdm`.`tweet_id`"),
+                    array(
+                        "token_id"    => $token_id
+                    ));
+    
+        if(!empty($data))
+            return $data[0]->count;
+        else
+            return $data;
+    }
 }
