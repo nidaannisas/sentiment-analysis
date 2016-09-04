cb8b341e0c0784c4f0635512211d3d224a8f0c12
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-05 00:18:22 +0700

    idf model add function calculate idf in word

--- app/Models/IDF.php
+++ app/Models/IDF.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 Class IDF extends Model
 {
     protected $table = 'idf';
+
+    public static function calculateIDFWord($word)
+    {
+        $data = DB::select( DB::raw("SELECT idf FROM `bag-of-words` WHERE `word` = :word"),
+                    array(
+                        "word"    => $word
+                    ));
+    
+        if(!empty($data))
+            return $data[0]->idf;
+        else
+            return 0;
+    }
 }
