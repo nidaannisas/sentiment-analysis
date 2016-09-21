32b0cbae844985b1cc23054130584389571b788f
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-09-22 01:47:44 +0700

    dashboard static

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Tweet;
 
 class DashboardController extends Controller
 {
 
     public function index()
     {
-    	return view('dashboard');
+    	$tweet = Tweet::count();
+    	$positive = Tweet::where('sentiment_id', 1)->count();
+    	$negative = Tweet::where('sentiment_id', 2)->count();
+    	$neutral = Tweet::where('sentiment_id', 3)->count();
+    	
+    	return view('dashboard')
+    		->with('tweet', $tweet)
+    		->with("positive", $positive)
+    		->with("negative", $negative)
+    		->with("neutral", $neutral);
     }
 
 }
