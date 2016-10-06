257462ef245fc28b67e8494d89e396f3100d42da
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-10-07 00:48:27 +0700

    add getTweet

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -17,17 +17,24 @@ Class Tweet extends Model
     public static function countPositive()
     {
         $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tweets` WHERE sentiment_id = 1"));
-    
+
         if(!empty($data))
             return $data[0]->count;
         else
             return $data;
     }
 
+    public static function getTweets()
+    {
+        $data = DB::select( DB::raw("SELECT * FROM `tweets`"));
+
+        return $data;
+    }
+
     public static function countNegative()
     {
         $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tweets` WHERE sentiment_id = 2"));
-    
+
         if(!empty($data))
             return $data[0]->count;
         else
@@ -37,7 +44,7 @@ Class Tweet extends Model
     public static function countNeutral()
     {
         $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tweets` WHERE sentiment_id = 3"));
-    
+
         if(!empty($data))
             return $data[0]->count;
         else
