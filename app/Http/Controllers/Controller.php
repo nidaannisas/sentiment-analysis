98ea461f5772296fe17a8070aeda8c5c38ffa48d
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-10-06 19:25:25 +0700

    add binary search in class controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,25 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function BinarySearch($array, $key, $low, $high)
+    {
+        if( $low > $high ) // termination case
+        {
+            return -1;
+        }
+
+        $middle = intval( ( $low+$high )/2 ); // gets the middle of the array
+
+        if ( $array[$middle] == $key ) // if the middle is our key
+        {
+            return $middle;
+        }
+        elseif ( $key < $array[$middle] ) // our key might be in the left sub-array
+        {
+            return $this->BinarySearch( $array, $key, $low, $middle-1 );
+        }
+
+        return $this->BinarySearch( $array, $key, $middle+1, $high ); // our key might be in the right sub-array
+    }
 }
