d7efdb5804e0d2db0523b79be1676fb83e9af8ea
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-10-18 17:19:01 +0700

    to lower normalization word

--- app/Models/NormalizationWord.php
+++ app/Models/NormalizationWord.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 Class NormalizationWord extends Model
 {
     protected $table = 'normalization-words';
+
+    public static function getNormalizationWords()
+    {
+        $data = DB::select( DB::raw("SELECT * FROM `normalization-words`"));
+
+        return $data;
+    }
 
 }
