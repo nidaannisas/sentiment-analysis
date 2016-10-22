da3885f9a8d3bc5768d3ac4064a149530943f45d
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-10-22 16:17:39 +0700

    call pembagian data seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(SentimentsTableSeeder::class);
+        $this->call(PembagianDataTableSeeder::class);
     }
 }
