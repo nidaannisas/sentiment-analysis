f6b688f6ae10178b0ae3bbe81227bf8b5e4748c9
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-10-23 12:52:07 +0700

    tokenize test and train

--- app/Http/Controllers/TokenizingController.php
+++ app/Http/Controllers/TokenizingController.php
@@ -20,14 +20,12 @@ class TokenizingController extends Controller
             ->with('words', $words);
     }
 
-    public function tokenize()
+    public function tokenizeTweetTest()
     {
-        // underscore masih kehapus
-
         // delete all field
         DB::table('bag-of-words')->delete();
 
-        $tweets = Tweet::all();
+        $tweets = Tweet::getTest();
 
         foreach($tweets as $tweet)
         {
@@ -132,7 +130,125 @@ class TokenizingController extends Controller
 
             DB::commit();
         }
+    }
 
+    public function tokenizeTweetTrain()
+    {
+        // delete all field
+        DB::table('bag-of-words')->delete();
+
+        $tweets = Tweet::getTrain();
+
+        foreach($tweets as $tweet)
+        {
+            // remove except letter
+            //$tweet->tweet = preg_replace('#^https?://*/', '', $tweet->tweet);
+            $tweet->tweet = preg_replace(array('/[^a-zA-Z_ -]/', '/[ -]+/', '/^-|-$/', '#^https?([a-zA-Z_ -]*)#'), array('', ' ', ''), $tweet->tweet);
+
+
+            // to lower
+            $tweet->tweet = strtolower($tweet->tweet);
+
+            $words = array();
+            $delim = " \n.,;-()";
+            $tok = strtok($tweet->tweet, $delim);
+            while ($tok !== false)
+            {
+                $words[] = $tok;
+                $tok = strtok($delim);
+            }
+
+            // unique di dalam dokumen
+            //$words = array_unique($words);
+
+            // unique antar dokument
+            // tidak ada redundant kata dalam tdm
+
+            $unique_words = array();
+
+            DB::beginTransaction();
+
+            foreach($words as $word)
+            {
+                $kata = BagOfWord::search($word);
+
+                if(empty($kata))
+                {
+                    $save = new BagOfWord;
+                    $save->word = $word;
+                    $save->count = 1;
+
+                    // unique word
+                    if(!in_array($word, $unique_words))
+                    {
+                        $save->count_tweet = 1;
+                        $unique_words[] = $word;
+                    }
+
+                    if($tweet->sentiment_id == 1)
+                    {
+                        $save->count_positive = 1;
+                    }
+                    else if($tweet->sentiment_id == 2)
+                    {
+                        $save->count_negative = 1;
+                    }
+                    else
+                    {
+                        $save->count_neutral = 1;
+                    }
+
+                    $save->save();
+
+                    // $tdm = new TDM;
+                    // $tdm->tweet_id = $tweet->id;
+                    // $tdm->token_id = $save->id;
+                    // $tdm->save();
+
+
+                }
+                else
+                {
+                    // $tdm = new TDM;
+                    // $tdm->tweet_id = $tweet->id;
+                    // $tdm->token_id = $kata->id;
+                    // $tdm->save();
+                    $save = BagOfWord::find($kata->id);
+                    $save->count = $save->count + 1;
+
+                    // unique word
+                    if(!in_array($word, $unique_words))
+                    {
+                        $save->count_tweet = $save->count_tweet + 1;
+                        $unique_words[] = $word;
+                    }
+
+                    if($tweet->sentiment_id == 1)
+                    {
+                        $save->count_positive = $save->count_positive + 1;
+                    }
+                    else if($tweet->sentiment_id == 2)
+                    {
+                        $save->count_negative = $save->count_negative + 1;
+                    }
+                    else
+                    {
+                        $save->count_neutral = $save->count_neutral + 1;
+                    }
+
+                    $save->save();
+                }
+            }
+
+            DB::commit();
+        }
+    }
+
+    public function tokenize()
+    {
+        $this->tokenizeTweetTest();
+        $this->tokenizeTweetTrain();
+        
         return Redirect::to('dashboard/tokenizing');
     }
 }
