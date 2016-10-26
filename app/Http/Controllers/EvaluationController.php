774b7678e8b28ba17af7348fb476880128bf63b9
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-10-26 19:18:47 +0700

    evaluation evaluate extend naive bayes

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -11,7 +11,7 @@ use App\Http\Requests;
 
 use Redirect;
 
-class EvaluationController extends Controller
+class EvaluationController extends NaiveBayesController
 {
     public function index()
     {
@@ -38,152 +38,152 @@ class EvaluationController extends Controller
         return $temp_array;
     }
 
-    public function tokenizing($tweets)
-    {
-        $words = array();
-        $count_positive = 0;
-        $count_negative = 0;
-        $count_neutral = 0;
-
-        $count_tweet_positive = 0;
-        $count_tweet_negative = 0;
-        $count_tweet_neutral = 0;
-
-        $i = 0;
-        foreach($tweets as $tweet)
-        {
-            if($tweet->sentiment_id == 1)
-                $count_tweet_positive++;
-            else if($tweet->sentiment_id == 2)
-                $count_tweet_negative++;
-            else
-                $count_tweet_neutral++;
-
-            // remove except letter
-            //$tweet->tweet = preg_replace('#^https?://*/', '', $tweet->tweet);
-            $tweet->tweet = preg_replace(array('/[^a-zA-Z_ -]/', '/[ -]+/', '/^-|-$/', '#^https?([a-zA-Z_ -]*)#'), array('', ' ', ''), $tweet->tweet);
-
-            // to lower
-            $tweet->tweet = strtolower($tweet->tweet);
-
-            $delim = " \n.,;-()";
-            $tok = strtok($tweet->tweet, $delim);
-
-            while ($tok !== false)
-            {
-                $words[$i]['term'] = $tok;
-                $tok = strtok($delim);
-
-                if($tweet->sentiment_id == 1)
-                {
-                    $words[$i]['count_positive'] = $count_positive + 1;
-                }
-                else if($tweet->sentiment_id == 2)
-                {
-                    $words[$i]['count_negative'] = $count_negative + 1;
-                }
-                else
-                {
-                    $words[$i]['count_neutral'] = $count_neutral + 1;
-                }
-
-                $i++;
-            }
-
-
-        }
-
-        $words = $this->quicksort_multidimension($words, 'term');
-
-
-        // $words = array_values($words);
-
-        $result = array("words" => $words,
-                        "count_positive" => $count_positive,
-                        "count_negative" => $count_negative,
-                        "count_neutral" => $count_neutral,
-                        "count_tweet_positive" => $count_tweet_positive = 0,
-                        "count_tweet_negative" => $count_tweet_negative = 0,
-                        "count_tweet_neutral" => $count_tweet_neutral = 0,
-                    );
-
-        return $result;
-    }
-
-    public function normalization($tweets)
-    {
-        $normalizations = NormalizationWord::all();
-
-        foreach($normalizations as $normalization)
-        {
-            $search = $this->BinarySearch($tweets, $normalization->word, 0, count($tweets)-1);
-            if($search > -1)
-            {
-                $tweets[$search] = $normalization->normal_word;
-            }
-
-        }
-
-        return $tweets;
-    }
-
-    public function stopword($tweets)
-    {
-        $stopwords = Stopword::all();
-
-        foreach($stopwords as $stopword)
-        {
-            $search = $this->BinarySearch($tweets, $stopword->word, 0, count($tweets)-1);
-            if($search > -1)
-            {
-                unset($tweets[$search]);
-                $tweets = array_values($tweets);
-            }
-        }
-
-        return $tweets;
-    }
-
-    public function naiveBayes($data)
-    {
-        $p_positive = $count_tweet_positive/$N;
-        $p_negative = $count_tweet_negative/$N;
-        $p_neutral = $count_tweet_neutral/$N;
-
-        // size vocabulary
-        $v = count($words);
-
-        foreach($test as $tweet)
-        {
-            // calculate positive
-            foreach($tweet as $word)
-            {
-                $p_word = ($count_positive + 1)/(BagOfWord::countWord($word) + $v);
-                $p_positive = $p_positive * $p_word;
-            }
-
-            // calculate negative
-            foreach($tweet as $word)
-            {
-                $p_word = ($count_negative + 1)/(BagOfWord::countWord($word) + $v);
-                $p_negative = $p_negative * $p_word;
-            }
-
-            // calculate neutral
-            foreach($tweet as $word)
-            {
-                $p_word = ($count_ + 1)/(BagOfWord::countWord($word) + $v);
-                $p_neutral = $p_neutral * $p_word;
-            }
-
-            if($p_positive > $p_negative && $p_positive > $p_neutral)
-                return 1;   // positive
-            else if($p_negative > $p_positive && $p_negative > $p_neutral)
-                return 2;   // negative
-            else
-                return 3;   // neutral
-        }
-    }
+    // public function tokenizing($tweets)
+    // {
+    //     $words = array();
+    //     $count_positive = 0;
+    //     $count_negative = 0;
+    //     $count_neutral = 0;
+    //
+    //     $count_tweet_positive = 0;
+    //     $count_tweet_negative = 0;
+    //     $count_tweet_neutral = 0;
+    //
+    //     $i = 0;
+    //     foreach($tweets as $tweet)
+    //     {
+    //         if($tweet->sentiment_id == 1)
+    //             $count_tweet_positive++;
+    //         else if($tweet->sentiment_id == 2)
+    //             $count_tweet_negative++;
+    //         else
+    //             $count_tweet_neutral++;
+    //
+    //         // remove except letter
+    //         //$tweet->tweet = preg_replace('#^https?://*/', '', $tweet->tweet);
+    //         $tweet->tweet = preg_replace(array('/[^a-zA-Z_ -]/', '/[ -]+/', '/^-|-$/', '#^https?([a-zA-Z_ -]*)#'), array('', ' ', ''), $tweet->tweet);
+    //
+    //         // to lower
+    //         $tweet->tweet = strtolower($tweet->tweet);
+    //
+    //         $delim = " \n.,;-()";
+    //         $tok = strtok($tweet->tweet, $delim);
+    //
+    //         while ($tok !== false)
+    //         {
+    //             $words[$i]['term'] = $tok;
+    //             $tok = strtok($delim);
+    //
+    //             if($tweet->sentiment_id == 1)
+    //             {
+    //                 $words[$i]['count_positive'] = $count_positive + 1;
+    //             }
+    //             else if($tweet->sentiment_id == 2)
+    //             {
+    //                 $words[$i]['count_negative'] = $count_negative + 1;
+    //             }
+    //             else
+    //             {
+    //                 $words[$i]['count_neutral'] = $count_neutral + 1;
+    //             }
+    //
+    //             $i++;
+    //         }
+    //
+    //
+    //     }
+    //
+    //     $words = $this->quicksort_multidimension($words, 'term');
+    //
+    //
+    //     // $words = array_values($words);
+    //
+    //     $result = array("words" => $words,
+    //                     "count_positive" => $count_positive,
+    //                     "count_negative" => $count_negative,
+    //                     "count_neutral" => $count_neutral,
+    //                     "count_tweet_positive" => $count_tweet_positive = 0,
+    //                     "count_tweet_negative" => $count_tweet_negative = 0,
+    //                     "count_tweet_neutral" => $count_tweet_neutral = 0,
+    //                 );
+    //
+    //     return $result;
+    // }
+    //
+    // public function normalization($tweets)
+    // {
+    //     $normalizations = NormalizationWord::all();
+    //
+    //     foreach($normalizations as $normalization)
+    //     {
+    //         $search = $this->BinarySearch($tweets, $normalization->word, 0, count($tweets)-1);
+    //         if($search > -1)
+    //         {
+    //             $tweets[$search] = $normalization->normal_word;
+    //         }
+    //
+    //     }
+    //
+    //     return $tweets;
+    // }
+    //
+    // public function stopword($tweets)
+    // {
+    //     $stopwords = Stopword::all();
+    //
+    //     foreach($stopwords as $stopword)
+    //     {
+    //         $search = $this->BinarySearch($tweets, $stopword->word, 0, count($tweets)-1);
+    //         if($search > -1)
+    //         {
+    //             unset($tweets[$search]);
+    //             $tweets = array_values($tweets);
+    //         }
+    //     }
+    //
+    //     return $tweets;
+    // }
+    //
+    // public function naiveBayes($data)
+    // {
+    //     $p_positive = $count_tweet_positive/$N;
+    //     $p_negative = $count_tweet_negative/$N;
+    //     $p_neutral = $count_tweet_neutral/$N;
+    //
+    //     // size vocabulary
+    //     $v = count($words);
+    //
+    //     foreach($test as $tweet)
+    //     {
+    //         // calculate positive
+    //         foreach($tweet as $word)
+    //         {
+    //             $p_word = ($count_positive + 1)/(BagOfWord::countWord($word) + $v);
+    //             $p_positive = $p_positive * $p_word;
+    //         }
+    //
+    //         // calculate negative
+    //         foreach($tweet as $word)
+    //         {
+    //             $p_word = ($count_negative + 1)/(BagOfWord::countWord($word) + $v);
+    //             $p_negative = $p_negative * $p_word;
+    //         }
+    //
+    //         // calculate neutral
+    //         foreach($tweet as $word)
+    //         {
+    //             $p_word = ($count_ + 1)/(BagOfWord::countWord($word) + $v);
+    //             $p_neutral = $p_neutral * $p_word;
+    //         }
+    //
+    //         if($p_positive > $p_negative && $p_positive > $p_neutral)
+    //             return 1;   // positive
+    //         else if($p_negative > $p_positive && $p_negative > $p_neutral)
+    //             return 2;   // negative
+    //         else
+    //             return 3;   // neutral
+    //     }
+    // }
 
     public function evaluateKFold()
     {
@@ -225,5 +225,26 @@ class EvaluationController extends Controller
         $time_elapsed_secs = microtime(true) - $start;
 
         echo ' '.$time_elapsed_secs;
+    }
+
+    public function evaluate()
+    {
+        $tweets = Tweet::getTrain();
+
+        $right_class = 0;
+        $N = count($tweets);
+
+        foreach($tweets as $tweet)
+        {
+            $class = $this->naiveBayes($tweet->tweet);
+
+            if($class == $tweet->sentiment_id)
+                $right_class++;
+        }
+
+        $accuracy = $right_class/$N;
+
+        return $accuracy*100;
+
     }
 }
