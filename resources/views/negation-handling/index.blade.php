6a64b7336afb5c10d19e1ac2d31c91f28230c0a3
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-11-07 22:28:45 +0700

    add type tweet negation handling

--- resources/views/negation-handling/index.blade.php
+++ resources/views/negation-handling/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 	<div class="row">
 		<ol class="breadcrumb">
 			<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
 			<li class="active">Negation Handling</li>
 		</ol>
 	</div><!--/.row-->
-	
+
 	<div class="row" style="margin-top : 20px;">
 		<div class="col-lg-12">
 			<div class="panel panel-default">
@@ -19,23 +19,24 @@
 						<h3>Proses Negation</h3>
 						<p>Klik tombol proses untuk memproses negation handling.</p>
 
-						<form role="form" action="{{ URL::to('dashboard/negation-handling/process') }} " method="post" style="padding-top : 20px;">	
+						<form role="form" action="{{ URL::to('dashboard/negation-handling/process') }} " method="post" style="padding-top : 20px;">
 							<input type="hidden" name="_token" value="{{ csrf_token() }}">
-							<div class="form-group"> 
+							<div class="form-group">
 							<button type="submit" class="btn btn-success"><span class="glyphicons glyphicons-disk-open"></span> Proses</button>
 						</div>
 					</div>
 					<div class="col-md-6" style="padding-bottom: 20px;">
-						
+
 					</div>
 					<hr style="color: black; width: 100%;">
 					<div class="col-md-12">
 						<table data-toggle="table"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
 						    <thead>
 						    <tr>
-						        <th data-field="state" data-checkbox="true">Item ID</th>
-						        <th data-field="name"  data-sortable="true" style="width: 80%;"">Tweet</th>
-						        <th data-field="price" data-sortable="true">Sentiment</th>
+						        <th data-field="id" data-checkbox="true">Item ID</th>
+						        <th data-field="tweet"  data-sortable="true" style="width: 80%;">Tweet</th>
+						        <th data-field="sentiment" data-sortable="true">Sentiment</th>
+                                <th data-field="type" data-sortable="true">Type</th>
 						    </tr>
 						    </thead>
 						    <tbody>
@@ -44,6 +45,7 @@
 						    	<td>{{ $tweet->id }}</td>
 						    	<td>{{ $tweet->tweet }}</td>
 						    	<td>{{ $tweet->sentiment->name }}</td>
+                                <td>{{ $tweet->type }}</td>
 							</tr>
 							@endforeach
 						    </tbody>
@@ -52,9 +54,9 @@
 				</div>
 			</div>
 		</div>
-	</div><!--/.row-->	
-	
-	
+	</div><!--/.row-->
+
+
 </div><!--/.main-->
 
 <link href="{{ URL::to('css/bootstrap-table.css') }}" rel="stylesheet">
@@ -65,9 +67,9 @@
 <script src="{{ URL::to('js/bootstrap-table.js') }}"></script>
 <script>
 	!function ($) {
-		$(document).on("click","ul.nav li.parent > a > span.icon", function(){		  
-			$(this).find('em:first').toggleClass("glyphicon-minus");	  
-		}); 
+		$(document).on("click","ul.nav li.parent > a > span.icon", function(){
+			$(this).find('em:first').toggleClass("glyphicon-minus");
+		});
 		$(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
 	}(window.jQuery);
 
@@ -77,6 +79,6 @@
 	$(window).on('resize', function () {
 	  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
 	})
-</script>	
+</script>
 
 @stop
