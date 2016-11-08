ece851802ad45e806b8c1396d196c5e4aa35ad55
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-11-08 14:47:42 +0700

    comment naive bayes tokenize

--- app/Http/Controllers/NaiveBayesController.php
+++ app/Http/Controllers/NaiveBayesController.php
@@ -37,49 +37,49 @@ class NaiveBayesController extends StopwordController
     	//return Redirect::to('dashboard/naive-bayes');
     }
 
-    public function tokenize($tweet)
-    {
-        // remove except letter
-        $tweet = preg_replace(array('/[^a-zA-Z_ -]/', '/[ -]+/', '/^-|-$/'), array('', ' ', ''), $tweet);
-
-        // to lower
-        $tweet = strtolower($tweet);
-
-        $words = array();
-        $delim = " \n.,;-()";
-        $tok = strtok($tweet, $delim);
-        while ($tok !== false)
-        {
-            $words[] = $tok;
-            $tok = strtok($delim);
-        }
-
-        // unique di dalam dokumen
-        //$words = array_unique($words);
-
-        return $words;
-    }
-
-    public function normalizeWord($tweet, $normalizations)
-    {
-        foreach($tweet as $key => $word)
-        {
-            $search = $this->BinarySearchObjectWord($normalizations, $word, 0, count($normalizations)-1);
-
-            $data = NormalizationWord::search($word);
-            if($search == -1)
-            {
-                if(!empty($data))
-                    echo $search.' '.$word.'<br />';
-            }
-            if($search > -1)
-            {
-                $tweet[$key] = $normalizations[$search]->normal_word;
-            }
-        }
-
-        return $tweet;
-    }
+    // public function tokenize($tweet)
+    // {
+    //     // remove except letter
+    //     $tweet = preg_replace(array('/[^a-zA-Z_ -]/', '/[ -]+/', '/^-|-$/'), array('', ' ', ''), $tweet);
+    //
+    //     // to lower
+    //     $tweet = strtolower($tweet);
+    //
+    //     $words = array();
+    //     $delim = " \n.,;-()";
+    //     $tok = strtok($tweet, $delim);
+    //     while ($tok !== false)
+    //     {
+    //         $words[] = $tok;
+    //         $tok = strtok($delim);
+    //     }
+    //
+    //     // unique di dalam dokumen
+    //     //$words = array_unique($words);
+    //
+    //     return $words;
+    // }
+    //
+    // public function normalizeWord($tweet, $normalizations)
+    // {
+    //     foreach($tweet as $key => $word)
+    //     {
+    //         $search = $this->BinarySearchObjectWord($normalizations, $word, 0, count($normalizations)-1);
+    //
+    //         $data = NormalizationWord::search($word);
+    //         if($search == -1)
+    //         {
+    //             if(!empty($data))
+    //                 echo $search.' '.$word.'<br />';
+    //         }
+    //         if($search > -1)
+    //         {
+    //             $tweet[$key] = $normalizations[$search]->normal_word;
+    //         }
+    //     }
+    //
+    //     return $tweet;
+    // }
 
     public function stopwordRemoval($tweet, $stopwords)
     {
