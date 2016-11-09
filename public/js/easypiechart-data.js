c71729058302e69a1375018a78edf1a2a2515d90
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-11-09 09:41:47 +0700

    piechart data

--- public/js/easypiechart-data.js
+++ public/js/easypiechart-data.js
@@ -26,5 +26,47 @@ $(function() {
    });
 });
 
+$(function() {
+   $('#precision-positive').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+
+$(function() {
+    $('#precision-negative').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+    $('#precision-neutral').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+   $('#recall-positive').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+
+$(function() {
+    $('#recall-negative').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+    $('#recall-neutral').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
 $('#calendar').datepicker({
 	});
