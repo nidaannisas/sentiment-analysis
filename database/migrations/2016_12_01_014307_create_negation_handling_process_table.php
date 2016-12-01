7cf538105054582442244688beec4a698df76d89
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-12-01 16:51:48 +0700

    negation handling table update

--- database/migrations/2016_12_01_014307_create_negation_handling_process_table.php
+++ database/migrations/2016_12_01_014307_create_negation_handling_process_table.php
@@ -15,9 +15,18 @@ class CreateNegationHandlingProcessTable extends Migration
     {
         Schema::create('negation_handling_process', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('tweet_negated_train')->unsigned()->default(0);
+            $table->integer('tweet_negated_test')->unsigned()->default(0);
             $table->integer('count_negated_positive')->unsigned()->default(0);
             $table->integer('count_negated_negative')->unsigned()->default(0);
             $table->integer('count_negated_neutral')->unsigned()->default(0);
+            $table->integer('count_negated_positive_result')->unsigned()->default(0);
+            $table->integer('count_negated_negative_result')->unsigned()->default(0);
+            $table->integer('count_negated_positive_test')->unsigned()->default(0);
+            $table->integer('count_negated_negative_test')->unsigned()->default(0);
+            $table->integer('count_negated_neutral_test')->unsigned()->default(0);
+            $table->integer('count_negated_positive_result_test')->unsigned()->default(0);
+            $table->integer('count_negated_negative_result_test')->unsigned()->default(0);
             $table->integer('count_negated_term')->unsigned()->nullable();
             $table->integer('count_negated_term_selection')->unsigned()->nullable();
             $table->float('process_time')->default(0);
