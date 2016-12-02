aedd48fc33ae969361780b53d743f90686975e55
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-12-02 18:43:26 +0700

    bag of words comment unused function

--- app/Models/BagOfWord.php
+++ app/Models/BagOfWord.php
@@ -23,57 +23,57 @@ Class BagOfWord extends Model
             return $data;
     }
 
-    public static function count($token_id)
-    {
-        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm` WHERE token_id = :token_id "),
-                    array(
-                        "token_id"    => $token_id
-                    ));
-
-        if(!empty($data))
-            return $data[0]->count;
-        else
-            return 0;
-    }
-
-    public static function countPositive($token_id)
-    {
-        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 1 AND `tweets`.`id` = `tdm`.`tweet_id`"),
-                    array(
-                        "token_id"    => $token_id
-                    ));
-
-        if(!empty($data))
-            return $data[0]->count;
-        else
-            return 0;
-    }
-
-    public static function countNegative($token_id)
-    {
-        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 2 AND `tweets`.`id` = `tdm`.`tweet_id`"),
-                    array(
-                        "token_id"    => $token_id
-                    ));
-
-        if(!empty($data))
-            return $data[0]->count;
-        else
-            return 0;
-    }
-
-    public static function countNeutral($token_id)
-    {
-        $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 3 AND `tweets`.`id` = `tdm`.`tweet_id`"),
-                    array(
-                        "token_id"    => $token_id
-                    ));
-
-        if(!empty($data))
-            return $data[0]->count;
-        else
-            return 0;
-    }
+    // public static function count($token_id)
+    // {
+    //     $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm` WHERE token_id = :token_id "),
+    //                 array(
+    //                     "token_id"    => $token_id
+    //                 ));
+    //
+    //     if(!empty($data))
+    //         return $data[0]->count;
+    //     else
+    //         return 0;
+    // }
+    //
+    // public static function countPositive($token_id)
+    // {
+    //     $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 1 AND `tweets`.`id` = `tdm`.`tweet_id`"),
+    //                 array(
+    //                     "token_id"    => $token_id
+    //                 ));
+    //
+    //     if(!empty($data))
+    //         return $data[0]->count;
+    //     else
+    //         return 0;
+    // }
+    //
+    // public static function countNegative($token_id)
+    // {
+    //     $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 2 AND `tweets`.`id` = `tdm`.`tweet_id`"),
+    //                 array(
+    //                     "token_id"    => $token_id
+    //                 ));
+    //
+    //     if(!empty($data))
+    //         return $data[0]->count;
+    //     else
+    //         return 0;
+    // }
+    //
+    // public static function countNeutral($token_id)
+    // {
+    //     $data = DB::select( DB::raw("SELECT COUNT(*) as count FROM `tdm`, `tweets` WHERE `tdm`.token_id = :token_id AND `tweets`.`sentiment_id` = 3 AND `tweets`.`id` = `tdm`.`tweet_id`"),
+    //                 array(
+    //                     "token_id"    => $token_id
+    //                 ));
+    //
+    //     if(!empty($data))
+    //         return $data[0]->count;
+    //     else
+    //         return 0;
+    // }
 
     public static function countPositiveWord($word)
     {
