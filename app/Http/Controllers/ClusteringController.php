29ceee85ae0af5b75755e514049d2d4ecbbb2281
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-12-03 07:41:55 +0700

    draft cluster dtm

--- app/Http/Controllers/ClusteringController.php
+++ app/Http/Controllers/ClusteringController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Sentiment;
 use App\Models\Tweet;
+use App\Models\BagOfWord;
 use App\Models\TweetResult;
 use App\Http\Requests;
 use Excel;
 use Redirect;
 use Input;
 
-class ClusteringController extends Controller
+class ClusteringController extends TokenizingController
 {
     public function index()
     {
@@ -27,10 +28,49 @@ class ClusteringController extends Controller
     {
         $tweets = TweetResult::all();
 
-        // jadiin token dulu
-        return $tweets[0];
+        foreach($tweets as $key => $tweet)
+        {
+            $tweets[$key]->tweet = $this->tokenizeEvaluation($tweets[$key]->tweet);
+            $tweets[$key]->tweet_unique = array_count_values($tweets[$key]->tweet);
+        }
 
-        // quick sort dan binary search bag of words
+        $bows = BagOfWord::getBagOfWords();
+        $bows = $this->quicksort_multidimension_object_word($bows);
+
+        // foreach($bows as $bow)
+        // {
+        //     $normal = $this->BinarySearchObjectWord($bows, $bow->word, 0, count($bows)-1);
+        //
+        //     if($normal == -1)
+        //         echo $bow->word.'<br />';
+        //     //if($bow->word == "partai") echo $bow->word;
+        // }
+
+        //var_dump($bows[0]->word);
+        // echo $normal = $this->BinarySearchObjectWord($bows, "partai", 0, count($bows)-1);
+        var_dump($tweets[0]->tweet_unique);
+        // echo $result = isset($tweets[0]->tweet_unique["partai"]) ? $tweets[0]->tweet_unique["partai"] : 0;
+
+
+        $dtm = array();
+
+        foreach($tweets as $i => $tweet)
+        {
+            foreach($bows as $j => $bow)
+            {
+                $result = isset($tweets[$i]->tweet_unique[$bows[$j]->word]) ? $tweets[$i]->tweet_unique[$bows[$j]->word] : 0;
+                $dtm[$i][$j] = $result;
+            }
+        }
+
+        for($i = 0; $i < 1; $i++)
+        {
+            foreach($bows as $j => $bow)
+            {
+                echo $dtm[$i][$j].' ';
+            }
+            echo '<br />';
+        }
     }
 
 }
